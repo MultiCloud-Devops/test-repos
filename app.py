0abc3549d82f97b89fa7f5a06446a7d9201c3982
MultiCloud-Devops <krishnaurs2022@gmail.com>
2025-04-07 15:04:44 +0530

    intial file

---
+++ app.py
@@ -0,0 +1,24 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    client = boto3.client('ec2')
+    response = client.run_instances(
+    ImageId='ami-076c6dbba59aa92e6',
+    InstanceType='t2.micro',
+    KeyName='Cust-KeyPair',
+    MaxCount=1,
+    MinCount=1,
+    TagSpecifications=[
+        {
+            'ResourceType': 'instance',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': 'server_from_lambda_boto3'
+                },
+            ]
+        },
+    ]
+)
+
